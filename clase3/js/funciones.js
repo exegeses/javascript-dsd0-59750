8c59475ed5e5db8fcc62f4646079e7fbe12dfed3
Marcos <exegeses@gmail.com>
2022-04-05 21:58:52 -0300

    práctica con funciones

---
+++ clase3/js/funciones.js
@@ -0,0 +1,15 @@
+//ubicamos elemento/s dentro del DOM
+let caja = document.getElementById('caja');
+
+//modificamos atributos: altura, ancho, color de fondo
+/*
+caja.style.height = '150px';
+caja.style.width = '95px';
+caja.style.backgroundColor = '#addf5e';
+*/
+function modificarAtr()
+{
+    caja.style.height = '150px';
+    caja.style.width = '95px';
+    caja.style.backgroundColor = '#addf5e';
+}
---
+++ clase3/js/mostarrOcultar.js
@@ -0,0 +1,12 @@
+//ubicamos elemento/s dentro del DOM
+let caja = document.getElementById('caja');
+
+//declaramos función/es de control
+function ocultar()
+{
+    caja.style.display = 'none';
+}
+function mostrar()
+{
+    caja.style.display = 'block';
+}
