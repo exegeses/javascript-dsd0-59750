c8c8d501ed417dc05271241043bfc60d6970dbc3
Marcos <exegeses@gmail.com>
2022-04-19 20:29:50 -0300

    pasar parámetros a funciones

---
+++ clase5/js/parametros.js
@@ -0,0 +1,8 @@
+//ubicar elemento/s dentro del DOM
+let caja = document.getElementById('caja');
+
+//declaramos función/es de control
+function cambiarColor( codigo )
+{
+    caja.style.backgroundColor = codigo;
+}
